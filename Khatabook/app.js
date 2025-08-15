939e4ae918dd0023d5ca15399cfab48bffb0af79
AmithKumar29 <amith3683@gmail.com>
2025-08-15 22:51:25 +0530

    app.js

--- Khatabook/app.js
+++ Khatabook/app.js
@@ -1,61 +1,86 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const fs=require('fs');
+const fs = require('fs');
 
+// Path to "hisaab" folder inside project
+const hisaabPath = path.join(__dirname, 'hisaab');
+
+// Create "hisaab" folder if it doesn't exist
+if (!fs.existsSync(hisaabPath)) {
+  fs.mkdirSync(hisaabPath, { recursive: true });
+  console.log(`Created folder: ${hisaabPath}`);
+}
+
+// EJS setup
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+// Middleware
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 
+// Home - List all files
 app.get('/', (req, res) => {
-    fs.readdir(`./hisaab`,function(err,files){
-        if(err) return res.status(500).send(err);
-        res.render("index",{files:files});
-    })
+  fs.readdir(hisaabPath, (err, files) => {
+    if (err) return res.status(500).send(err);
+    res.render('index', { files });
+  });
 });
 
+// Create page
 app.get('/create', (req, res) => {
-    res.render('create');
+  res.render('create');
 });
 
-app.post("/createhisaab",function(req,res){
-    var cd=new Date();
-    var date=`${cd.getDate()}-${cd.getMonth()+1}-${cd.getFullYear()}`
-    fs.writeFile(`./hisaab/${date}.txt`,req.body.content,function(err){
-        if(err) return res.status(500).send(err);
-        res.redirect("/");
-    });
+// Create file
+app.post('/createhisaab', (req, res) => {
+  const cd = new Date();
+  const date = `${cd.getDate()}-${cd.getMonth() + 1}-${cd.getFullYear()}`;
+  const filePath = path.join(hisaabPath, `${date}.txt`);
+  
+  fs.writeFile(filePath, req.body.content, (err) => {
+    if (err) return res.status(500).send(err);
+    res.redirect('/');
+  });
+});
 
-})
-
+// Edit page
 app.get('/edit/:filename', (req, res) => {
-    fs.readFile(`./hisaab/${req.params.filename}`,"utf-8",function(err,filedata){
-        if(err) return res.status(500).send(err);
-        res.render("edit",{filedata,filename:req.params.filename});
-    })
+  const filePath = path.join(hisaabPath, req.params.filename);
+  fs.readFile(filePath, 'utf-8', (err, filedata) => {
+    if (err) return res.status(500).send(err);
+    res.render('edit', { filedata, filename: req.params.filename });
+  });
 });
 
-app.post("/update/:filename",(req,res)=>{
-    fs.writeFile(`./hisaab/${req.params.filename}`,req.body.content,function(err){
-        if(err) return res.status(500).send(err);
-        res.redirect("/");
-    })
-})
+// Update file
+app.post('/update/:filename', (req, res) => {
+  const filePath = path.join(hisaabPath, req.params.filename);
+  fs.writeFile(filePath, req.body.content, (err) => {
+    if (err) return res.status(500).send(err);
+    res.redirect('/');
+  });
+});
 
-app.get("/hisaab/:filename",function(req,res){
-    fs.readFile(`./hisaab/${req.params.filename}`,"utf-8",function(err,filedata){
-        if(err) return res.status(500).send(err);
-        res.render("hisaab",{filedata,filename:req.params.filename});
-    })
-})
+// View file
+app.get('/hisaab/:filename', (req, res) => {
+  const filePath = path.join(hisaabPath, req.params.filename);
+  fs.readFile(filePath, 'utf-8', (err, filedata) => {
+    if (err) return res.status(500).send(err);
+    res.render('hisaab', { filedata, filename: req.params.filename });
+  });
+});
 
-app.get("/delete/:filename",function(req,res){
-     fs.unlink(`./hisaab/${req.params.filename}`,function(err){
-        if(err) return res.status(500).send(err);
-        res.redirect("/");
-    })
-})
+// Delete file
+app.get('/delete/:filename', (req, res) => {
+  const filePath = path.join(hisaabPath, req.params.filename);
+  fs.unlink(filePath, (err) => {
+    if (err) return res.status(500).send(err);
+    res.redirect('/');
+  });
+});
 
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
